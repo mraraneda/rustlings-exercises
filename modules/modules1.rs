e0c78141950b715b96458c3f8e2be433d664efb7
Marco Araneda <marcoarfts@gmail.com>
2024-05-15 21:14:01 -0400

    advance update - hashmaps1

--- modules/modules1.rs
+++ modules/modules1.rs
@@ -3,20 +3,19 @@
 // Execute `rustlings hint modules1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 mod sausage_factory {
     // Don't let anybody outside of this module see this!
     fn get_secret_recipe() -> String {
         String::from("Ginger")
     }
 
-    fn make_sausage() {
+    pub fn make_sausage() {
         get_secret_recipe();
         println!("sausage!");
     }
 }
 
 fn main() {
+    /// main puede acceder a los módulos que estan en el mismo archivo
     sausage_factory::make_sausage();
 }
